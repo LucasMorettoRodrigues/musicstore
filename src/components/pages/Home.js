46484ecb12f072e7ff9e6ec630a01c2cbe35752d
Lucas <r.lucasmoretto@gmail.com>
2022-03-14 11:12:49 -0300

    fix slider

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,15 +13,22 @@ import { useRef, useState } from 'react'
 const Home = () => {
 
     const listRef = useRef()
+    const productsContainer = useRef()
     const [position, setPosition] = useState(0)
 
     const handleScroll = (direction) => {
-        console.log(listRef.current.getBoundingClientRect());
+
+        let maxSlide = 1
+
+        if (productsContainer.current.getBoundingClientRect().width < 1409 &&
+            productsContainer.current.getBoundingClientRect().width > 1090) maxSlide = 2
+        if (productsContainer.current.getBoundingClientRect().width <= 1090) maxSlide = 3
+
         if (direction === "left" && position < 0) {
             listRef.current.style.transform = `translateX(${position + 320}px)`
             setPosition(position + 320)
         }
-        if (direction === "right" && position > -320) {
+        if (direction === "right" && position > (-320 * maxSlide)) {
             listRef.current.style.transform = `translateX(${position - 320}px)`
             setPosition(position - 320)
         }
@@ -35,7 +42,7 @@ const Home = () => {
             </header>
             <section className={styles.section_container}>
                 <h1>Best Sellers</h1>
-                <div className={styles.products_container} >
+                <div className={styles.products_container} ref={productsContainer}>
                     <button onClick={() => handleScroll("left")} className={styles.prev_btn}>
                         <i class="bi bi-chevron-right"></i>
                     </button>
