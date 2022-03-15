7febd43597eacf901342bc1accbd8317d13ea89a
Lucas <r.lucasmoretto@gmail.com>
2022-03-14 21:52:55 -0300

    fix navigate at success page

--- src/components/pages/Success.js
+++ src/components/pages/Success.js
@@ -49,7 +49,7 @@ const Success = () => {
             {orderId
                 ? <p style={{ padding: 10, textAlign: "center" }}>Order has been created successfully. Your order number is ${orderId} </p>
                 : <p style={{ padding: 10, textAlign: "center" }} >Successfull. Your order is being prepared...</p>}
-            <button onClick={() => navigate("/")} style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+            <button onClick={() => navigate("/musicstore")} style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
         </div>
     );
 };
